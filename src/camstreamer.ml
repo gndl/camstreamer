8d888a7cc1de3b349b6742904a8e2196f3164c6d
GDL <gaetan.dubreil@sesam-vitale.fr>
2018-12-16 22:29:45 +0100

    Server part with Opium

--- src/camstreamer.ml
+++ src/camstreamer.ml
@@ -1,6 +1,6 @@
 open FFmpeg
 
-let () =
+let scan_cam () =
   if Array.length Sys.argv < 2 then Printf.(Av.Format.(
       printf "\ninput devices :\n";
       Avdevice.get_video_input_formats() |> List.iter
@@ -8,11 +8,9 @@ let () =
       printf "\noutput devices :\n";
       Avdevice.get_video_output_formats() |> List.iter
         (fun d -> printf"\t%s (%s)\n"(get_output_name d)(get_output_long_name d));
-      printf"\nusage: %s input [output]\ninput and output can be devices or file names\n" Sys.argv.(0);
       exit 0));
 
-  let src = try Avdevice.open_video_input Sys.argv.(1)
-    with Avutil.Failure _ -> Av.open_input Sys.argv.(1) in
+  let src = Avdevice.open_video_input Sys.argv.(1) in
 
   let (_, ias, _) = Av.find_best_video_stream src in
 
@@ -34,3 +32,25 @@ let () =
   Av.close dst;
 
 
+open Opium.Std
+
+let html_msg title msg =
+ "<html>" ^
+ "  <head>" ^
+ "    <title>Message</title>" ^
+ "  </head>" ^
+ "  <body>" ^
+ "    <h1>" ^ title ^ "  </h1>" ^
+      msg ^
+ "  </body>" ^
+ "</html>"
+
+
+ let hello = get "/" (fun _ ->
+   `Html (html_msg "Salut à toi" "ô mon frère")
+   |> respond')
+
+let _ =
+  App.empty
+   |> hello
+|> App.run_command
